7f89c310c7ab1694b86241ac111a173a9d88f173
cacatem <caua.cavalcante@outlook.com>
2023-12-24 09:57:18 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function hideElement() {
-    var x = document.getElementById("mobMenu");
-    if (window.getComputedStyle(x).display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
